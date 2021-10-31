dc094dd2a16f796fed507321f809afaaf59e3375
FengchunFan <77016886+FengchunFan@users.noreply.github.com>
2021-10-30 20:26:28 -0700

    Update ffan005_lab8_part4.c

--- ffan005_lab8_part4.c
+++ ffan005_lab8_part4.c
@@ -6,7 +6,7 @@
  *
  *      I acknowledge all content contained herein, excluding template or example
  *      code, is my own original work.
- *      Demo:
+ *      Demo: https://www.youtube.com/shorts/Zdr6xMpz10w
  */
 #include <avr/io.h>
 #ifdef _SIMULATE_

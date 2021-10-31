f790e9e0b263c6eb8fc309b740296da85b085e3c
FengchunFan <77016886+FengchunFan@users.noreply.github.com>
2021-10-30 18:44:34 -0700

    Create ffan005_lab8_part3.c

---
+++ ffan005_lab8_part3.c
@@ -0,0 +1,46 @@
+/*      Author: ffan005
+ *  Partner(s) Name: 
+ *      Lab Section:
+ *      Assignment: Lab #  Exercise #
+ *      Exercise Description: [optional - include for your own benefit]
+ *
+ *      I acknowledge all content contained herein, excluding template or example
+ *      code, is my own original work.
+ *      Demo: https://www.youtube.com/watch?v=8UF-zKVhmqk
+ */
+
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+//unsigned short x = ADC; give errors :(
+
+void ADC_init(){
+ADCSRA |= (1 <<  ADEN) | (1 << ADSC) | (1 << ADATE);
+}
+
+int main(void) {
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRB = 0xFF; PORTB = 0x00;
+    DDRD = 0xFF; PORTD = 0x00;
+
+    ADC_init();
+
+    while(1){
+    unsigned short x = ADC;
+    //using PB0 as the indicator as shown in the demo video
+
+    unsigned short PB = (char)x;
+    if(PB >= (767/2)){
+    PORTB = 0x01;
+    }else{
+    PORTB = 0x00;
+    }
+
+    /*unsigned short PD = (char)(x >> 8);
+    PORTB = PB;
+    PORTD = PD;*/
+    }
+    return 0;
+}
